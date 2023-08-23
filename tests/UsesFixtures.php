822ee7424a97c6e291b0219500de20c97d7e173e
Willem ter Berg <wrpterberg@gmail.com>
2023-08-23 13:54:52 +0200

    WIP

--- tests/UsesFixtures.php
+++ tests/UsesFixtures.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * This file is part of the WterBerg/PHPStan-Laravel package.
+ * This file is part of the wterberg/phpstan-laravel package.
  *
  * This source file is subject to the license that is
  * bundled with this source code in the LICENSE.md file.
@@ -11,12 +11,10 @@ declare(strict_types=1);
 
 namespace Tests;
 
-class ClassWithEnvFunctionCall
+trait UsesFixtures
 {
-    public $property;
-
-    public function __construct()
+    private function fixturePath(string $file): string
     {
-        $this->property = env('TZ');
+        return __DIR__ . '/fixtures/' . $file;
     }
 }
